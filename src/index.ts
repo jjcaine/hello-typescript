3d157cda8941f72b324841705f209ef695e17121
John Caine <john@zivianhealth.com>
2023-09-09 10:59:06 -0400

    through basic types lesson

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-let age: number = 20;
-if (age < 50) age += 10;
-console.log(`New age: ${age}`);
+// tuples - fixed
+let user: [number, string] = [1, "John"];
+
+//functions
+function calculateTax(income: number, taxYear: number): number {
+  if (taxYear < 2022) return income * 1.2;
+  return income * 1.3;
+}
+
+//objects
+// can annotate objects
+let employee: {
+  readonly id: number;
+  name: string;
+} = { id: 1, name: "John" };
+
+employee.name = "John Caine";
